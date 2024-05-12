e6e244d311379658da9cc09b60c1645a84978928
zachjhavers <zachjhavers@gmail.com>
2024-05-12 15:47:45 -0400

    Got Rid Of Web Vitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Analytics } from "@vercel/analytics/react";
 
@@ -13,5 +12,3 @@ root.render(
     <Analytics />
   </React.StrictMode>
 );
-
-reportWebVitals();
